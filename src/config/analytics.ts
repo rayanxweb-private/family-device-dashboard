b1e5c1bb799ac0aabb40754788960721bee7d9f8
rayanxweb-private <aryudhaamanaronian@gmail.com>
2026-06-03 16:25:01 +0700

    Create analytics.ts

---
+++ src/config/analytics.ts
@@ -0,0 +1,22 @@
+import { getAnalytics, isSupported, Analytics } from 'firebase/analytics';
+import { initializeApp, getApps, getApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+};
+
+export const initAnalytics = async (): Promise<Analytics | null> => {
+  if (typeof window !== 'undefined') {
+    const supported = await isSupported();
+    if (supported) {
+      const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
+      return getAnalytics(app);
+    }
+  }
+  return null;
+};
